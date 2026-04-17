5a245a2122ee14419161994671f1535b9c1aaae5
Utkarsh025025 <uagarwal055@gmail.com>
2026-04-18 01:40:19 +0530

    Delete drizzle directory

--- drizzle/0002_careless_karen_page.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE `golfScores` ADD CONSTRAINT `userDateUnique` UNIQUE(`userId`,`scoreDate`);--> statement-breakpoint
-ALTER TABLE `userCharities` ADD CONSTRAINT `userCharities_userId_unique` UNIQUE(`userId`);--> statement-breakpoint
-ALTER TABLE `userCharities` ADD CONSTRAINT `userCharityUnique` UNIQUE(`userId`);
